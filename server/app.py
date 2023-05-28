bca3a7b13b3aade04c60a691553affa95fc19d5a
Nicholas <crazedglitch@yahoo.com>
2023-05-28 17:31:52 -0500

    lab complete

--- server/app.py
+++ server/app.py
@@ -44,6 +44,33 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch( self, id ):
+        plant = Plant.query.filter( Plant.id == id ).first()
+        plantData = request.get_json()
+
+        if 'is_in_stock' in plantData:
+            plant.is_in_stock = plantData[ 'is_in_stock' ]
+
+        db.session.commit()
+
+        response_dict = plant.to_dict()
+        response = make_response( response_dict, 200 )
+
+        return response
+    
+    def delete( self, id ):
+
+        plant = Plant.query.filter( Plant.id == id ).first()
+
+        db.session.delete( plant )
+        db.session.commit()
+
+        response_dict = { "message": "record successfully deleted." }
+
+        response = make_response( response_dict, 204 )
+
+        return response
 
 api.add_resource(PlantByID, '/plants/<int:id>')
         
